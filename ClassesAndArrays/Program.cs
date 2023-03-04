f561b783095f5156165603ff3ce22011c2414176
Xennoxx <108221381+Xennoxx@users.noreply.github.com>
2023-03-04 23:26:30 +0100

    Created a new program to understand the relationship between arrays and classes better

---
+++ ClassesAndArrays/Program.cs
@@ -0,0 +1,23 @@
+﻿using ClassesAndArrays;
+
+class Program
+{
+    static void main(string[] args)
+    {
+
+
+        static void Main(string[] args)
+        {
+            Console.Write("Bitte wählen Sie die Anzahl der Spieler: ");
+            int playeranz = Convert.ToInt32(Console.ReadLine());
+            Spieler[] sp = new Spieler[playeranz];
+            for (int a = 1; a <= playeranz; a++)
+            {
+                Console.Write("Spieler" + a + " Name: ");
+                string input = Console.ReadLine();
+                sp[a] = new Spieler(input);
+                Console.WriteL‚‚ine("Danke. Du heißt jetzt " + sp[a].name + ".");
+            }
+        }
+    }
+}
---
+++ ClassesAndArrays/Spieler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace ClassesAndArrays
+{
+	public class Spieler
+	{
+        public Spieler(string name)
+        {
+            this.name = name;
+        }
+        public string name;
+        }
+}
+
